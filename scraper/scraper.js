aead34f185fd9076b0d7e77afd43cbf6b2c49d54
idoganz6 <92280482+idoganz6@users.noreply.github.com>
2021-12-06 12:01:31 +0700

    Update scraper.js

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -83,56 +83,6 @@ function xnxx(q) {
 	})
 }
 
-function javhd(q) {
-	return new Promise(async (resolve, reject) => {
-		await axios.get(`https://javhdd.net/search/${q}`, {
-			headers: {
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-			}
-		}).then(respon => {
-			const $ = cheerio.load(respon.data)
-			const hasil = []
-			$('#content-thumbs > div.mozaique.cust-nb-cols').each(function (tyu, zu) {
-				$(zu).find('div').each(function (chu, chuwi) {
-					const Url = $(chuwi).find('div.thumb-inside > div > a').attr('href') || undefined
-					const thumb = $(chuwi).find('div.thumb-inside > div > a > img').attr('data-src') || undefined
-					const judul = $(chuwi).find('div.thumb-under > p:nth-child(1) > a').text().trim() || undefined
-					const View = $(chuwi).find('div.thumb-under > p.metadata > span.right > span.superfluous').text().trim() || undefined
-					const Info = $(chuwi).find('div.thumb-under > p.metadata').text().trim() || undefined
-					const result = {
-						url: Url,
-						judul: judul,
-						thumb: thumb,
-						view: View,
-						info: Info
-					}
-					hasil.push(result)
-				})
-			})
-			const hasil2 = []
-			hasil.map(itzy => {
-				if (itzy.url === undefined) return 
-				if (itzy.judul === undefined) return 
-				if (itzy.thumb === undefined) return
-				if (itzy.view === undefined) return
-				if (itzy.info === undefined) return
-				const Format = {
-					judul: itzy.judul,
-					url: 'https://javhdd.net' + itzy.url,
-					thumb: itzy.thumb,
-					viewers: itzy.view,
-					info: itzy.info
-				}
-				hasil2.push(Format)
-			})
-			const data = {
-				status: respon.status,
-				result: hasil2
-			}
-			resolve(data)
-		}).catch(reject)
-	})
-}
 
 function wallpaper(title, page = '1') {
     return new Promise((resolve, reject) => {
@@ -168,6 +118,57 @@ function wikimedia(title) {
             resolve(hasil)
         })
     })
+}
+
+function javhd(q) {
+	return new Promise(async (resolve, reject) => {
+		await axios.get(`https://javhdd.net/search/${q}`, {
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+			}
+		}).then(respon => {
+			const $ = cheerio.load(respon.data)
+			const hasil = []
+			$('#content-thumbs > div.mozaique.cust-nb-cols').each(function (tyu, zu) {
+				$(zu).find('div').each(function (chu, rani) {
+					const Url = $(rani).find('div.thumb-inside > div > a').attr('href') || undefined
+					const thumb = $(rani).find('div.thumb-inside > div > a > img').attr('data-src') || undefined
+					const judul = $(rani).find('div.thumb-under > p:nth-child(1) > a').text().trim() || undefined
+					const View = $(rani).find('div.thumb-under > p.metadata > span.right > span.superfluous').text().trim() || undefined
+					const Info = $(rani).find('div.thumb-under > p.metadata').text().trim() || undefined
+					const result = {
+						url: Url,
+						judul: judul,
+						thumb: thumb,
+						view: View,
+						info: Info
+					}
+					hasil.push(result)
+				})
+			})
+			const hasil2 = []
+			hasil.map(ido => {
+				if (ido.url === undefined) return 
+				if (ido.judul === undefined) return 
+				if (ido.thumb === undefined) return
+				if (ido.view === undefined) return
+				if (ido.info === undefined) return
+				const Format = {
+					judul: ido.judul,
+					url: 'https://javhdd.net' + ido.url,
+					thumb: ido.thumb,
+					viewers: ido.view,
+					info: ido.info
+				}
+				hasil2.push(Format)
+			})
+			const data = {
+				status: respon.status,
+				result: hasil2
+			}
+			resolve(data)
+		}).catch(reject)
+	})
 }
 
 function porno() {
